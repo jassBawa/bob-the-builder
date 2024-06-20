0bf269541cb16e063fe89ca0b676dc499ff697a7
jassbawa <jasspreetsbawa@gmail.com>
2024-06-21 00:15:40 +0530

    fix: style changes

--- app/(dashboard)/dashboard/page.js
+++ app/(dashboard)/dashboard/page.js
@@ -16,8 +16,8 @@ function Page() {
 
   return (
     <>
-      <div className="mt-16 mx-8 rounded bg-white">
-        <h2 className="text-2xl font-semibold">Welcome back, Lorem ipsum</h2>
+      <div className="mt-16 mx-8 rounded bg-white p-4">
+        <h2 className="text-2xl font-semibold">Welcome back</h2>
         <p className="text-sm">Track and Manage your information</p>
       </div>
       <div className="mt-4 mx-8 p-8 rounded bg-white">
